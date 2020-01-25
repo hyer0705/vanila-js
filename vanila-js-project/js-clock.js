858cd2520386dcef2bf36cba1bacec6997a0e459
hyer0705 <hyer1205@gmail.com>
2020-01-25 17:50:22 +0900

    Making-a-JS-Clock-part-Two

--- vanila-js-project/js-clock.js
+++ vanila-js-project/js-clock.js
@@ -6,10 +6,11 @@ function getTime() {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000);
 }
 init();
